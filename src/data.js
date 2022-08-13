ee59867dc9003cd77c1ba9e0b198cc4d47f35a36
Carl <craklax@gmail.com>
2022-08-13 13:36:56 -0300

    Desafios + Rediseño

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const list = [
+  {
+    id: 1,
+    title: "Ender Lilies: Quietus of the Knights",
+    author: "by Live Wire, Adglobe | Jun 21, 2021",
+    price: 15,
+    img: "https://image.api.playstation.com/vulcan/ap/rnd/202105/2006/UQ0eSe7Rx4K0sXkO1nR7Iq9k.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "Celeste",
+    author: "by Extremely OK Games, Ltd.  | 25 Jan, 2018",
+    price: 10,
+    img: "https://cdn-products.eneba.com/resized-products/z_swcbaAyfrXQFyuBqp4-r37yc72giflCpNSM0JBYI0_350x200_3x-0.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Danganronpa Decadence",
+    author: "by Chunsoft | 3 Dic, 2021",
+    price: 40,
+    img: "https://m.media-amazon.com/images/I/61tmyuD+sXL.jpg",
+    amount: 1,
+  },
+  
+];
+
+export default list;
